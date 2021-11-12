1652059d700819e8a35ffff1c4724e7cb17047c6
alvinnasa <alvinnasa5@gmail.com>
2021-11-12 21:17:47 +0700

    initial commit

---
+++ js/scripts1.js
@@ -0,0 +1,4 @@
+
+* Start Bootstrap - Landing Page v6.0.4 (https://startbootstrap.com/theme/landing-page)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
